53a079b266bfecf48cba0aee3736248d1f831d4a
nepallium <122187419+nepallium@users.noreply.github.com>
2026-07-21 23:12:12 -0400

    finish most of renderer (no z idx)

--- main.cpp
+++ main.cpp
@@ -10,151 +10,163 @@
 #include <sstream>
 #include <string>
 
-constexpr TGAColor white = {255, 255, 255, 255}; // attention, BGRA order
-constexpr TGAColor green = {0, 255, 0, 255};
-constexpr TGAColor red = {0, 0, 255, 255};
-constexpr TGAColor blue = {255, 128, 64, 255};
-constexpr TGAColor yellow = {0, 200, 255, 255};
+constexpr TGAColor white = { 255, 255, 255, 255 }; // attention, BGRA order
+constexpr TGAColor green = { 0, 255, 0, 255 };
+constexpr TGAColor red = { 0, 0, 255, 255 };
+constexpr TGAColor blue = { 255, 128, 64, 255 };
+constexpr TGAColor yellow = { 0, 200, 255, 255 };
 
 namespace Canvas {
-constexpr int width{64};
-constexpr int height{64};
-constexpr double midWidth{(width - 1) / 2};
-constexpr double midHeight{(width - 1) / 2};
-TGAImage framebuffer(width, height, TGAImage::RGB);
+	constexpr int width{ 800 };
+	constexpr int height{ 800 };
+	constexpr float midWidth{ (width - 1) / 2 };
+	constexpr float midHeight{ (height - 1) / 2 };
+	TGAImage framebuffer(width, height, TGAImage::RGB);
 } // namespace Canvas
 
-void line(int ax, int ay, int bx, int by, TGAImage &framebuffer,
-          TGAColor color) {
-  // check for steep vertical
-  bool steep{};
-  if (std::abs(by - ay) > std::abs(bx - ax)) {
-    steep = true;
-    std::swap(ax, ay);
-    std::swap(bx, by);
-  }
+void line(int ax, int ay, int bx, int by, TGAImage& framebuffer,
+	TGAColor color) {
+	// check for steep vertical
+	bool steep{ std::abs(by - ay) > std::abs(bx - ax) };
+	if (steep) {
+		std::swap(ax, ay);
+		std::swap(bx, by);
+	}
 
-  if (ax > bx) {
-    std::swap(ax, bx);
-    std::swap(ay, by);
-  }
+	if (ax > bx) {
+		std::swap(ax, bx);
+		std::swap(ay, by);
+	}
 
-  int y{ay};
-  int ierror{};
-  for (int x = ax; x <= bx; ++x) {
-    if (steep) {
-      framebuffer.set(y, x, color);
-    } else {
-      framebuffer.set(x, y, color);
-    }
+	int y{ ay };
+	int ierror{ 0 };
+	for (int x = ax; x <= bx; ++x) {
+		if (steep) {
+			framebuffer.set(y, x, color);
+		}
+		else {
+			framebuffer.set(x, y, color);
+		}
 
-    ierror += 2 * (std::abs(by - ay));
-    y += by > ay ? 1 : -1 * (ierror > (bx - ax));
-    ierror -= 2 * (bx - ax) * (ierror > (bx - ax));
-  }
+		ierror += 2 * (std::abs(by - ay));
+		y += (by > ay ? 1 : -1) * (ierror > (bx - ax));
+		ierror -= 2 * (bx - ax) * (ierror > (bx - ax));
+	}
 }
 
 class Vertex {
-  int x_{};
-  int y_{};
-  double z_{};
+	int x_{};
+	int y_{};
+	double z_{};
 
 public:
-  Vertex(int x, int y, double z)
-      : x_{x}
-      , y_{y}
-      , z_{z} {}
+	Vertex(int x, int y, double z)
+		: x_{ x }
+		, y_{ y }
+		, z_{ z } {
+	}
 
-  Vertex() = default;
+	Vertex() = default;
 
-  int x() const { return x_; }
+	int x() const { return x_; }
 
-  int y() const { return y_; }
+	int y() const { return y_; }
 
-  double z() const { return z_; }
+	double z() const { return z_; }
 
-  void x(int x) { x_ = x; }
+	void x(int x) { x_ = x; }
 
-  void y(int y) { y_ = y; }
+	void y(int y) { y_ = y; }
 
-  void z(double z) { z_ = z; }
+	void z(double z) { z_ = z; }
 };
 
 class Face {
-  std::vector<Vertex> vertices_{};
+	std::vector<Vertex> vertices_{};
 
 public:
-  Face(const std::vector<Vertex> &vertices)
-      : vertices_{vertices} {}
+	Face(const std::vector<Vertex>& vertices)
+		: vertices_{ vertices } {
+	}
 
-  const std::vector<Vertex> &getVertices() const { return vertices_; }
+	const std::vector<Vertex>& getVertices() const { return vertices_; }
 
-  void setVertices(const std::vector<Vertex> &vertices) {
-    vertices_ = vertices;
-  }
+	void setVertices(const std::vector<Vertex>& vertices) {
+		vertices_ = vertices;
+	}
 
-  void drawFaceBorders() {
-    line(vertices_[0].x(), vertices_[0].y(), vertices_[1].x(), vertices_[1].y(),
-         Canvas::framebuffer, red);
-    line(vertices_[0].x(), vertices_[0].y(), vertices_[2].x(), vertices_[2].y(),
-         Canvas::framebuffer, red);
-    line(vertices_[2].x(), vertices_[2].y(), vertices_[1].x(), vertices_[1].y(),
-         Canvas::framebuffer, red);
-  }
+	void drawFaceBorders() {
+		line(vertices_[0].x(), vertices_[0].y(), vertices_[1].x(), vertices_[1].y(),
+			Canvas::framebuffer, red);
+		line(vertices_[0].x(), vertices_[0].y(), vertices_[2].x(), vertices_[2].y(),
+			Canvas::framebuffer, red);
+		line(vertices_[2].x(), vertices_[2].y(), vertices_[1].x(), vertices_[1].y(),
+			Canvas::framebuffer, red);
+	}
 };
 
 int convertModelCoordsToWorld(double model, int mid) {
-  return std::round(model * 2 * mid + mid);
+	return std::round(mid * (1 + model));
 }
 
-int main(int argc, char **argv) {
-  std::ifstream objFile("obj/floor.obj");
+int main(int argc, char** argv) {
+	std::ifstream objFile(R"(C:\Users\Alex\Documents\Alex Stuff\Programming\c++\tinyrenderer\obj\diablo3_pose\diablo3_pose.obj)");
 
-  std::vector<Vertex> vertices{};
+	if (!objFile.is_open()) {
+		// This will print the exact reason (e.g., "No such file or directory")
+		std::perror("File open failed");
+	}
 
-  std::string curLine{};
-  std::istringstream stream{};
+	std::vector<Vertex> vertices{};
 
-  while (objFile.good()) {
-    std::getline(objFile, curLine);
-    stream.str(curLine);
+	std::string curLine{};
+	std::string lineStart{};
 
-    std::string lineStart{};
-    std::getline(stream, lineStart, ' ');
+	int i{ 0 };
+	while (std::getline(objFile, curLine)) {
+		if (curLine.empty()) {
+			continue;
+		}
 
-    if (lineStart == "v") {
-      // v 0.608654 -0.568839 -0.416318
-      double x{};
-      double y{};
-      double z{};
+		std::istringstream stream{};
+		stream.str(curLine);
 
-      stream >> x >> y >> z;
+		stream >> lineStart;
 
-      vertices.push_back({convertModelCoordsToWorld(x, Canvas::midWidth),
-                          convertModelCoordsToWorld(y, Canvas::midHeight), z});
+		if (lineStart == "v") {
+			// v 0.608654 -0.568839 -0.416318
+			double x{};
+			double y{};
+			double z{};
 
-    } else if (lineStart == "f") {
-      // f 1193/1240/1193 1180/1227/1180 1179/1226/1179
-      std::string vCluster{}; // "v1/v2/v3"
-      while (std::getline(stream, vCluster)) {
-        std::istringstream vClusterStream(vCluster);
-        std::vector<int> vIndices{};
-        std::string token;
+			stream >> x >> y >> z;
 
-        while (std::getline(vClusterStream, token)) {
-          vIndices.push_back(std::stoi(token) - 1);
-        }
+			vertices.push_back({ convertModelCoordsToWorld(x, Canvas::midWidth),
+								convertModelCoordsToWorld(y, Canvas::midHeight), z });
 
-        // assumes vertices already read
-        Face face({vertices[vIndices[0]], vertices[vIndices[1]],
-                   vertices[vIndices[2]]});
-        face.drawFaceBorders();
-      }
-    }
-  }
+		}
+		else if (lineStart == "f") {
+			// f 1193/1240/1193 1180/1227/1180 1179/1226/1179
+			std::string vCluster{}; // "v/vt/vn"
+			std::vector<int> vIndices{};
+			while (stream >> vCluster) {
+				std::istringstream vClusterStream(vCluster);
+				std::string token;
 
-  objFile.close();
+				std::getline(vClusterStream, token, '/');
+				vIndices.push_back(std::stoi(token) - 1);
 
-  Canvas::framebuffer.write_tga_file("framebuffer.tga");
-  return 0;
+
+			}
+			// assumes vertices already read
+			Face face({ vertices[vIndices[0]], vertices[vIndices[1]],
+						vertices[vIndices[2]] });
+			face.drawFaceBorders();
+		}
+	}
+
+	objFile.close();
+
+	Canvas::framebuffer.write_tga_file("framebuffer.tga");
+	return 0;
 }

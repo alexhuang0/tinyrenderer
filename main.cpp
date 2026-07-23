d5bdcbbe3be1995b7c29e322ec0417c80d9ce024
nepallium <122187419+nepallium@users.noreply.github.com>
2026-07-22 21:10:45 -0400

    insertion sort

--- main.cpp
+++ main.cpp
@@ -20,22 +20,37 @@ constexpr TGAColor red = { 0, 0, 255, 255 };
 constexpr TGAColor blue = { 255, 128, 64, 255 };
 constexpr TGAColor yellow = { 0, 200, 255, 255 };
 
+template <typename T, int N>
+std::array<T, N> insertionSort(std::array<T, N> arr) {
+
+	T tmp{};
+	for (std::size_t i{ 1 }; i < N; ++i) {
+		auto curIdx{ i };
+		tmp = arr[i];
+		while (curIdx > 0 && arr[curIdx - 1] > tmp) {
+			arr[curIdx] = arr[curIdx - 1];
+			--curIdx;
+		}
+		arr[curIdx] = tmp;
+	}
+
+	return arr;
+}
 
 int main(int argc, char** argv) {
-	Model model(R"(C:\Users\Alex\Documents\Alex Stuff\Programming\c++\tinyrenderer\obj\diablo3_pose\diablo3_pose.obj)");
+	//Model model(R"(C:\Users\Alex\Documents\Alex Stuff\Programming\c++\tinyrenderer\obj\diablo3_pose\diablo3_pose.obj)");
+	std::array arr{ 5, 2, 4, 6, 1 };
+	arr = insertionSort(arr);
+	for (int element : arr) {
+		std::cout << element << " ";
+	}
 
 
 	TGAImage framebuffer(Canvas::width, Canvas::height, TGAImage::RGB);
 
-	for (std::size_t i{ 0 }; i < model.nfaces(); ++i) {
-		Renderer::drawFaceTBorders(model, i, framebuffer, red);
-	}
-
-	for (std::size_t i{ 0 }; i < model.nverts(); ++i) {
-		const vec3& vert{ model.vert(i) };
-		auto [ax, ay] = Renderer::project(vert);
-		framebuffer.set(ax, ay, white);
-	}
+	Renderer::triangle(7, 45, 35, 100, 45, 60, framebuffer, red);
+	Renderer::triangle(120, 35, 90, 5, 45, 110, framebuffer, white);
+	Renderer::triangle(115, 83, 80, 90, 85, 120, framebuffer, green);
 
 	framebuffer.write_tga_file("framebuffer.tga");
 	return 0;

8e44c558ed5621cace0518d7b8d9ee11cab260de
nepallium <122187419+nepallium@users.noreply.github.com>
2026-07-25 11:26:14 -0400

    add basic vector operations

--- geometry.h
+++ geometry.h
@@ -14,6 +14,57 @@ template<int n> std::ostream& operator<<(std::ostream& out, const vec<n>& v) {
 	return out;
 }
 
+template<int n> vec<n> operator+(const vec<n>& lhs, const vec<n>& rhs) {
+	vec<n> res{};
+	for (int i{ 0 }; i < n; ++i) {
+		res[i] = lhs[i] + rhs[i];
+	}
+	return res;
+}
+
+template<int n> vec<n> operator-(const vec<n>& lhs, const vec<n>& rhs) {
+	vec<n> res{};
+	for (int i{ 0 }; i < n; ++i) {
+		res[i] = lhs[i] - rhs[i];
+	}
+	return res;
+}
+
+// scalar multiplication
+template<int n, typename T> vec<n> operator*(T scalar, const vec<n>& vect) {
+	vec<n> res{};
+	for (int i{ 0 }; i < n; ++i) {
+		res[i] = scalar * vect[i];
+	}
+	return res;
+}
+template<int n, typename T> vec<n> operator*(const vec<n>& vect, T scalar) {
+	vec<n> res{};
+	for (int i{ 0 }; i < n; ++i) {
+		res[i] = scalar * vect[i];
+	}
+	return res;
+}
+
+// dot product
+template<int n> double operator*(const vec<n>& lhs, const vec<n>& rhs) {
+	double res{};
+	for (int i{ 0 }; i < n; ++i) {
+		res += lhs[i] * rhs[i];
+	}
+	return res;
+}
+
+template<> struct vec<2> {
+	union {
+		struct { double x, y; };
+		double data[2];
+
+	};
+	double& operator[](const int i) { assert(i >= 0 && i < 2); return data[i]; }
+	double operator[](const int i) const { assert(i >= 0 && i < 2); return data[i]; }
+};
+
 template<> struct vec<3> {
 	union {
 		struct { double x, y, z; };
@@ -24,4 +75,18 @@ template<> struct vec<3> {
 	double operator[](const int i) const { assert(i >= 0 && i < 3); return data[i]; }
 };
 
+template<> struct vec<4> {
+	union {
+		struct { double x, y, z, w };
+		double data[4];
+
+	};
+	double& operator[](const int i) { assert(i >= 0 && i < 4); return data[i]; }
+	double operator[](const int i) const { assert(i >= 0 && i < 4); return data[i]; }
+};
+
+typedef vec<2> vec2;
 typedef vec<3> vec3;
+typedef vec<4> vec4;
+
+
--- main.cpp
+++ main.cpp
@@ -17,6 +17,12 @@
 
 
 int main(int argc, char** argv) {
+	//vector test
+	vec4 v1{ 3, 6, 2, -5 };
+	vec4 v2{ 10, 10, -3, 0 };
+	std::cout << v1 - v2;
+
+
 	TGAImage framebuffer(Canvas::width, Canvas::height, TGAImage::RGB);
 	TGAImage depthbuffer(Canvas::width, Canvas::height, TGAImage::GRAYSCALE);
 
@@ -28,17 +34,17 @@ int main(int argc, char** argv) {
 	//Renderer::triangle(ax, ay, 0, bx, by, 255, cx, cy, 0, framebuffer);
 	//Renderer::triangle(ax, ay, 0, bx, by, 0, cx, cy, 255, framebuffer);
 
-	Model model(R"(C:\Users\Alex\Documents\Alex Stuff\Programming\c++\tinyrenderer\obj\diablo3_pose\diablo3_pose.obj)");
-	for (int i{ 0 }; i < model.nfaces(); ++i) {
-		auto [ax, ay, az] = Renderer::project(model.vert(i, 0));
-		auto [bx, by, bz] = Renderer::project(model.vert(i, 1));
-		auto [cx, cy, cz] = Renderer::project(model.vert(i, 2));
-		TGAColor rnd;
-		for (int channel{ 0 }; channel < 3; ++channel) rnd[channel] = std::rand() % 255;
-		Renderer::triangle(ax, ay, az, bx, by, bz, cx, cy, cz, depthbuffer, framebuffer, rnd);
-	}
+	//Model model(R"(C:\Users\Alex\Documents\Alex Stuff\Programming\c++\tinyrenderer\obj\diablo3_pose\diablo3_pose.obj)");
+	//for (int i{ 0 }; i < model.nfaces(); ++i) {
+	//	auto [ax, ay, az] = Renderer::project(model.vert(i, 0));
+	//	auto [bx, by, bz] = Renderer::project(model.vert(i, 1));
+	//	auto [cx, cy, cz] = Renderer::project(model.vert(i, 2));
+	//	TGAColor rnd;
+	//	for (int channel{ 0 }; channel < 3; ++channel) rnd[channel] = std::rand() % 255;
+	//	Renderer::triangle(ax, ay, az, bx, by, bz, cx, cy, cz, depthbuffer, framebuffer, rnd);
+	//}
 
-	framebuffer.write_tga_file("framebuffer.tga");
-	depthbuffer.write_tga_file("zbuffer.tga");
+	//framebuffer.write_tga_file("framebuffer.tga");
+	//depthbuffer.write_tga_file("zbuffer.tga");
 	return 0;
 }

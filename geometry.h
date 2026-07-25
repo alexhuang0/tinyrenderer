86e321b3ee0b1d9170c4fedf24558d87ee715f70
nepallium <122187419+nepallium@users.noreply.github.com>
2026-07-25 12:25:10 -0400

    add operator assignment overload

--- geometry.h
+++ geometry.h
@@ -9,41 +9,43 @@ template<int n> struct vec {
 	double  operator[](const int i) const { assert(i >= 0 && i < n); return data[i]; }
 };
 
-template<int n> std::ostream& operator<<(std::ostream& out, const vec<n>& v) {
-	for (std::size_t i = 0; i < n; ++i) out << v[i] << " ";
-	return out;
+template<int n> vec<n>& operator+=(vec<n>& lhs, const vec<n>& rhs) {
+	for (int i{ 0 }; i < n; ++i) {
+		lhs[i] += rhs[i];
+	}
+	return lhs;
+}
+template<int n> vec<n>& operator-=(vec<n>& lhs, const vec<n>& rhs) {
+	for (int i{ 0 }; i < n; ++i) {
+		lhs[i] -= rhs[i];
+	}
+	return lhs;
+}
+template<int n> vec<n>& operator*=(vec<n>& lhs, const vec<n>& rhs) {
+	for (int i{ 0 }; i < n; ++i) {
+		lhs[i] *= rhs[i];
+	}
+	return lhs;
 }
 
-template<int n> vec<n> operator+(const vec<n>& lhs, const vec<n>& rhs) {
-	vec<n> res{};
-	for (int i{ 0 }; i < n; ++i) {
-		res[i] = lhs[i] + rhs[i];
-	}
-	return res;
+template<int n> vec<n> operator+(vec<n> lhs, const vec<n>& rhs) {
+	lhs += rhs;
+	return lhs;
 }
 
-template<int n> vec<n> operator-(const vec<n>& lhs, const vec<n>& rhs) {
-	vec<n> res{};
-	for (int i{ 0 }; i < n; ++i) {
-		res[i] = lhs[i] - rhs[i];
-	}
-	return res;
+template<int n> vec<n> operator-(vec<n> lhs, const vec<n>& rhs) {
+	lhs -= rhs;
+	return lhs;
 }
 
 // scalar multiplication
-template<int n, typename T> vec<n> operator*(T scalar, const vec<n>& vect) {
-	vec<n> res{};
-	for (int i{ 0 }; i < n; ++i) {
-		res[i] = scalar * vect[i];
-	}
-	return res;
+template<int n, typename T> vec<n> operator*(T scalar, vec<n> vect) {
+	vect *= scalar;
+	return vect;
 }
-template<int n, typename T> vec<n> operator*(const vec<n>& vect, T scalar) {
-	vec<n> res{};
-	for (int i{ 0 }; i < n; ++i) {
-		res[i] = scalar * vect[i];
-	}
-	return res;
+template<int n, typename T> vec<n> operator*(vec<n> vect, T scalar) {
+	vect *= scalar;
+	return vect;
 }
 
 // dot product
@@ -53,6 +55,11 @@ template<int n> double operator*(const vec<n>& lhs, const vec<n>& rhs) {
 		res += lhs[i] * rhs[i];
 	}
 	return res;
+}
+
+template<int n> std::ostream& operator<<(std::ostream& out, const vec<n>& v) {
+	for (std::size_t i = 0; i < n; ++i) out << v[i] << " ";
+	return out;
 }
 
 template<> struct vec<2> {
@@ -77,7 +84,7 @@ template<> struct vec<3> {
 
 template<> struct vec<4> {
 	union {
-		struct { double x, y, z, w };
+		struct { double x, y, z, w; };
 		double data[4];
 
 	};

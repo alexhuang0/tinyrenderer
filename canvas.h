16ad9c31025b4e0b7eb2536997b2dddbc982d458
nepallium <122187419+nepallium@users.noreply.github.com>
2026-08-02 12:57:18 -0400

    fix int division to float

--- canvas.h
+++ canvas.h
@@ -3,6 +3,6 @@
 namespace Canvas {
 	constexpr int width{ 800 };
 	constexpr int height{ 800 };
-	constexpr float midWidth{ (width - 1) / 2 };
-	constexpr float midHeight{ (height - 1) / 2 };
+	constexpr float midWidth{ (width - 1) / 2.f };
+	constexpr float midHeight{ (height - 1) / 2.f };
 } // namespace Canvas

f74f2f9071d13fbc3c6e933237e8ae6d19bfdf0e
nepallium <122187419+nepallium@users.noreply.github.com>
2026-08-30 15:03:12 -0400

    finish ao with basic cube sampling

--- main.cpp
+++ main.cpp
@@ -52,36 +52,53 @@ int main(int argc, char** argv) {
 	constexpr double radius{ 0.1 };
 	std::mt19937 mt{ 10 };
 	std::uniform_real_distribution<double> getDist{ -radius, radius };
-	constexpr int samples{ 128 };
+	constexpr int samples{ 512 };
 
 	const matrix<4, 4> eye_to_screen{ rContext.Viewport * rContext.Perspective };
 	const matrix<4, 4> screen_to_eye{ (eye_to_screen).inverse() };
 	for (int x{ 0 }; x < width; ++x) {
 		for (int y{ 0 }; y < height; ++y) {
-			const double z = rContext.zbuffer[y + x * width];
-			if (z < 1e-5) continue; // open sky / background, no occlusion
+			const double z = rContext.zbuffer[x + y * width];
+			if (z < -1e5) continue; // open sky / background, no occlusion
 
 			vec4 coords_eye_space{ screen_to_eye * vec4{
 				static_cast<double>(x), static_cast<double>(y), z, 1.
 			} };
+			coords_eye_space /= coords_eye_space.w;
 
 			// sampling
 			int voters{}; // valid, in-bound samples
 			int votes{}; // occluded samples
 			for (int i{ 0 }; i < samples; ++i) {
-				double dist{ getDist(mt) };
-				vec4 rand_coord{ x + dist, y + dist, z + dist, 1. };
+				vec4 rand_coord{
+					coords_eye_space.x + getDist(mt),
+					coords_eye_space.y + getDist(mt),
+					coords_eye_space.z + getDist(mt),
+					1.
+				};
 
+				vec4 rand_coord_screen{ eye_to_screen * rand_coord };
+				rand_coord_screen /= rand_coord_screen.w;
+				int sx = static_cast<int>(rand_coord_screen.x);
+				int sy = static_cast<int>(rand_coord_screen.y);
 				// out of bounds check
-				if (rand_coord.x < 0 || rand_coord.x > width
-					|| rand_coord.y < 0 || rand_coord.y > height)
+				if (sx < 0 || sx >= width
+					|| sy < 0 || sy >= height)
 				{
 					continue;
 				}
 
-				vec4 rand_coord_screen{ eye_to_screen * rand_coord };
-				if (rand_coord_screen.z < 1e-5) continue; // open sky / background, no occlusion
-				if (rand_coord_screen.z < rContext.zbuffer[rand_coord_screen.y + rand_coord_screen.x + width]) {
+				// the rendered on-screen zbuffer at the sample's x, y coords
+				double scene_z{ rContext.zbuffer[sx + sy * width] };
+				if (scene_z < -1e5) continue; // open sky / background, no occlusion
+
+
+				// range check to prevent dark halo
+				if (std::abs(z - scene_z) > 5. * radius) continue;
+
+				// rand_coord_screen.z is a rand generated z val in the surrounding cube
+				// if this rand z is 
+				if (rand_coord_screen.z < scene_z) {
 					++votes; // this sample is occluded
 				}
 
@@ -89,7 +106,7 @@ int main(int argc, char** argv) {
 			}
 
 			// scale down occlusion contribution
-			const double occlusion_factor{ 0.4 * votes / voters };
+			const double occlusion_factor{ (voters > 0) ? (0.4 * votes / voters) : 0. };
 			const double ambient{ 1. - occlusion_factor }; // how lit this point is (1 = max bright)
 			TGAColor screen_color{ finalbuffer.get(x, y) };
 			finalbuffer.set(x, y, TGAColor{
